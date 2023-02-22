19f522afbf911eecdcefb88c042d82f164de77f7
Vladimir <troyanskii1998@yandex.ru>
2023-02-22 22:54:47 +0300

    добавляет функцию для проверки является ли строка палиндромом

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,17 @@
 let checkStringLength = function (string, maxLength) {
   return (string.length <= maxLength) ? true : false;
 }
+
+//Функция для проверки, является ли строка палиндромом
+
+let checkStringPalin = function (string) {
+  string = string.replaceAll(' ', '');
+  string = string.toLowerCase();
+
+  let compareString = '';
+  for (let i = 0; i < string.length; i++) {
+    compareString += string.at(-i - 1);
+  }
+
+  return (string === compareString) ? true : false;
+}
