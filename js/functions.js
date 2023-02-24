ad1724ccb698f7828a103f9fa388089b23c79422
Vladimir <troyanskii1998@yandex.ru>
2023-02-24 14:59:10 +0300

    добавляет функцию для добавления в строку доп. символов

--- js/functions.js
+++ js/functions.js
@@ -31,3 +31,27 @@ let getNumbers = function (string) {
 
   return (exitNumber === '') ? NaN : Number(exitNumber);
 }
+
+//Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины
+
+let addSymbols = function (string, minLength, extraSymbols) {
+  let newString = '';
+
+  if (string.length >= minLength) {
+    return string;
+  } else {
+
+    for (let i = 0; i < (minLength - string.length) % extraSymbols.length ; i++) {
+      newString += extraSymbols.at(i);
+    }
+
+    for (let i = 0; i < Math.floor((minLength - string.length) / extraSymbols.length); i++) {
+      newString += extraSymbols;
+    }
+
+    newString += string;
+
+    return newString;
+
+  }
+}
