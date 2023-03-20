55f3e70dfa72aad1938cca64b130ed2e89fa366b
Vladimir <troyanskii1998@yandex.ru>
2023-03-20 22:23:49 +0300

    доработка закрытия и открытия окна с формой отправки фото

--- js/upload-image-modal.js
+++ js/upload-image-modal.js
@@ -1,5 +1,7 @@
 /* global Pristine:readonly */
 
+const body = document.querySelector('body');
+
 const modalUploadPhoto = document.querySelector('.img-upload__overlay');
 const buttonCloseModalUploadPhoto = modalUploadPhoto.querySelector('.img-upload__cancel');
 
@@ -20,11 +22,32 @@ photoUploadForm.addEventListener('submit', (evt) => {
   }
 });
 
+//Закрытие окна клавишей Esc
+import {isEscapeKey} from './functions.js';
+const onModalEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeModal();
+  }
+};
+
+//Закрытие окна
+const closeModal = () => {
+  modalUploadPhoto.classList.add('hidden');
+  body.classList.remove('modal-open');
+  inputUploadImage.value = null;
+
+  document.removeEventListener('keydown', onModalEscKeydown);
+  buttonCloseModalUploadPhoto.removeEventListener('click', closeModal);
+};
+
 //Открытие окна
 inputUploadImage.addEventListener('change', () => {
   modalUploadPhoto.classList.remove('hidden');
+  body.classList.add('modal-open');
 
-  buttonCloseModalUploadPhoto.addEventListener('click', () => {
-    modalUploadPhoto.classList.add('hidden');
-  });
+  buttonCloseModalUploadPhoto.addEventListener('click', closeModal);
+  document.addEventListener('keydown', onModalEscKeydown);
 });
+
+
